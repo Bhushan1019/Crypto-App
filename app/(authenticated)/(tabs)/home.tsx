84ee16110a5c460f550b61fa39664686602eaa73
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-15 16:02:44 +0530

    added transaction list

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -4,6 +4,8 @@ import Colors from "@/constants/Colors";
 import RoundBtn from "@/components/RoundBtn";
 import Dropdown from "@/components/Dropdown";
 import { useBalanceStore } from "@/store/balanceStore";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
   const { balance, runTransaction, transactions, clearTransactions } =
@@ -28,9 +30,43 @@ const Page = () => {
 
       <View style={styles.actionRow}>
         <RoundBtn icon={"add"} text={"Add Money"} onPress={onAddMoney} />
-        <RoundBtn icon={"refresh"} text={"Exchange"} />
+        <RoundBtn
+          icon={"refresh"}
+          text={"Exchange"}
+          onPress={clearTransactions}
+        />
         <RoundBtn icon={"list"} text={"Details"} />
         <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No Transactions yet
+          </Text>
+        )}
+        {transactions.map((transaction) => (
+          <View
+            key={transaction.id}
+            style={{ flexDirection: "row", alignItems: "center", gap: 16 }}
+          >
+            <View style={styles.circle}>
+              <Ionicons
+                name={transaction.amount > 0 ? "add" : "remove"}
+                size={24}
+                color={Colors.dark}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontWeight: "400" }}>{transaction.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {transaction.date.toLocaleDateString()}
+              </Text>
+            </View>
+            <Text>{transaction.amount}₹</Text>
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
@@ -58,6 +94,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    gap: 20,
+    borderRadius: 16,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 export default Page;
