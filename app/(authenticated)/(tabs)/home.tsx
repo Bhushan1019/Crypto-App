031fecf58440ebd2f9a4bf05fb15a06883d4e12e
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-15 13:31:33 +0530

    added balance screen

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
 import React from "react";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
+  const balance = 0;
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>₹</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <Button title="Send" />
+        <Button title="Request" />
+        <Button title="Request" />
+        <Button title="Request" />
+      </View>
+    </ScrollView>
   );
 };
-
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: {
+    fontSize: 60,
+    fontWeight: "bold",
+  },
+  currency: {
+    fontSize: 30,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
 export default Page;
