986301a2269352830e7a2f368185d8c2c33be148
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-16 14:37:49 +0530

    added api data

--- app/api/listings+api.ts
+++ app/api/listings+api.ts
@@ -2,21 +2,21 @@ const API_KEY = process.env.CRYPTO_API_KEY;
 
 export async function GET(request: Request) {
   // We Have ALready Got the data so we will use default data
-  // const url = new URL(request.url);
-  // const limit = url.searchParams.get("limit") || 5;
+  const url = new URL(request.url);
+  const limit = url.searchParams.get("limit") || 5;
 
-  // const response = await fetch(
-  //   `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=EUR`,
-  //   {
-  //     headers: {
-  //       "X-CMC_PRO_API_KEY": API_KEY!,
-  //     },
-  //   }
-  // );
+  const response = await fetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=EUR`,
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": API_KEY!,
+      },
+    }
+  );
 
-  // const res = await response.json();
-  // return Response.json(res.data);
-  return Response.json(data);
+  const res = await response.json();
+  return Response.json(res.data);
+  // return Response.json(data);
 }
 
 const data = [
