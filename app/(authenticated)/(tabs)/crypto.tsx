aae34f48aedcfaa809e4c42a748567ddd96cbe0d
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-16 14:34:12 +0530

    added style to crypto page

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -26,50 +26,58 @@ const Page = () => {
   });
 
   return (
-    // <ScrollView
-    //   style={{ backgroundColor: Colors.background }}
-    //   contentContainerStyle={{ paddingTop: headerHeight }}>
-    //   <Text style={defaultStyles.sectionHeader}>Latest Crypot</Text>
-    //   <View style={defaultStyles.block}>
-    //     {currencies.data?.map((currency: Currency) => (
-    //       <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
-    //         <TouchableOpacity style={{ flexDirection: 'row', gap: 14, alignItems: 'center' }}>
-    //           <Image source={{ uri: data?.[currency.id].logo }} style={{ width: 40, height: 40 }} />
-    //           <View style={{ flex: 1, gap: 6 }}>
-    //             <Text style={{ fontWeight: '600', color: Colors.dark }}>{currency.name}</Text>
-    //             <Text style={{ color: Colors.gray }}>{currency.symbol}</Text>
-    //           </View>
-    //           <View style={{ gap: 6, alignItems: 'flex-end' }}>
-    //             <Text>{currency.quote.EUR.price.toFixed(2)} €</Text>
-    //             <View style={{ flexDirection: 'row', gap: 4 }}>
-    //               <Ionicons
-    //                 name={currency.quote.EUR.percent_change_1h > 0 ? 'caret-up' : 'caret-down'}
-    //                 size={16}
-    //                 color={currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red'}
-    //               />
-    //               <Text
-    //                 style={{ color: currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red' }}>
-    //                 {currency.quote.EUR.percent_change_1h.toFixed(2)} %
-    //               </Text>
-    //             </View>
-    //           </View>
-    //         </TouchableOpacity>
-    //       </Link>
-    //     ))}
-    //   </View>
-    // </ScrollView>
-    <View>
-      {currencies.data?.map((currency: Currency) => (
-        <View style={{ flexDirection: "row" }} key={currency.id}>
-          <Image
-            source={{ uri: data?.[currency.id].logo }}
-            style={{ width: 32, height: 32 }}
-          />
-
-          <Text>{currency.name}</Text>
-        </View>
-      ))}
-    </View>
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{ paddingTop: headerHeight }}
+    >
+      <Text style={defaultStyles.sectionHeader}>Latest Crypto</Text>
+      <View style={defaultStyles.block}>
+        {currencies.data?.map((currency: Currency) => (
+          <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
+            <TouchableOpacity
+              style={{ flexDirection: "row", gap: 14, alignItems: "center" }}
+            >
+              <Image
+                source={{ uri: data?.[currency.id].logo }}
+                style={{ width: 40, height: 40 }}
+              />
+              <View style={{ flex: 1, gap: 6 }}>
+                <Text style={{ fontWeight: "600", color: Colors.dark }}>
+                  {currency.name}
+                </Text>
+                <Text style={{ color: Colors.gray }}>{currency.symbol}</Text>
+              </View>
+              <View style={{ gap: 6, alignItems: "flex-end" }}>
+                <Text>{currency.quote.EUR.price.toFixed(2)} €</Text>
+                <View style={{ flexDirection: "row", gap: 4 }}>
+                  <Ionicons
+                    name={
+                      currency.quote.EUR.percent_change_1h > 0
+                        ? "caret-up"
+                        : "caret-down"
+                    }
+                    size={16}
+                    color={
+                      currency.quote.EUR.percent_change_1h > 0 ? "green" : "red"
+                    }
+                  />
+                  <Text
+                    style={{
+                      color:
+                        currency.quote.EUR.percent_change_1h > 0
+                          ? "green"
+                          : "red",
+                    }}
+                  >
+                    {currency.quote.EUR.percent_change_1h.toFixed(2)} %
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </Link>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 export default Page;
